6b033562fd0569d609f18eb14fa04195fc04f616
Ashley Wong <ashwong04@gmail.com>
2018-08-30 15:50:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,22 +2,12 @@ def oxford_comma(array)
   
   if array.length == 1
     array[0]
-    #array.join()
   
   elsif array.length == 2
     array.join( " and ")
-  
-  # elsif array.length == 3
-  #   new_array = array.join(", ")
-  #     puts new_array
-  #   new_array.insert(-10, "and ")
-  #     puts new_array
-  #   new_array
     
   elsif array.length >= 3
     array[-1].insert(0, "and ")
     new_array = array.join(", ")
   end
 end
-
-
